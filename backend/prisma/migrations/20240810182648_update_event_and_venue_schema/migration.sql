1c08bbf9ece7ab1581677804e1c1085d3a6a3bdf
Naveen Kavitha Gunasekaran <kgnaveen2002@gmail.com>
2024-08-11 00:14:55 +0530

    GetHistory, GetEventsPosted, GetEventById update

---
+++ backend/prisma/migrations/20240810182648_update_event_and_venue_schema/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `eventsPostedId` JSON NULL;
---
+++ backend/src/util/emailUtils.js
@@ -0,0 +1,55 @@
+// utils/emailUtils.js
+
+const nodemailer = require('nodemailer');
+
+// Configure Nodemailer transport
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'lucizodi@gmail.com', // Replace with your email
+    pass: 'Velur@123'   // Replace with your email password
+  }
+});
+
+/**
+ * Send a purchase confirmation email to the user.
+ * @param {string} to - Recipient's email address.
+ * @param {Object} purchaseDetails - Details of the purchase.
+ */
+async function sendPurchaseConfirmationEmail(to, purchaseDetails) {
+  const { eventName, location, localDate, localTime, numberOfTickets, totalPrice } = purchaseDetails;
+
+  const mailOptions = {
+    from: 'lucizodi@gmail.com',
+    to: 'praveen1990179@gmail.com',
+    subject: 'Ticket Purchase Confirmation',
+    text: `
+      Dear User,
+
+      Thank you for your purchase!
+
+      Event: ${eventName}
+      Location: ${location}
+      Date: ${localDate.toLocaleDateString()}
+      Time: ${localTime}
+
+      Number of Tickets: ${numberOfTickets}
+      Total Price: $${totalPrice}
+
+      We hope you enjoy the event!
+
+      Best Regards,
+      Event Management Team
+    `
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Purchase confirmation email sent successfully.');
+  } catch (error) {
+    console.error('Error sending purchase confirmation email:', error);
+    throw new Error('Failed to send purchase confirmation email');
+  }
+}
+
+module.exports = { sendPurchaseConfirmationEmail };
