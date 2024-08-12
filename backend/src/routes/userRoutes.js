823226711ffd247e2c1e0709bf54d7d36b213188
Naveen Kavitha Gunasekaran <kgnaveen2002@gmail.com>
2024-08-12 10:28:25 +0530

    Added authentication to all routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -11,13 +11,13 @@ router.get('/authentication', checkJwt, userController.getAuthentication);
 // User Operations
 router.post('/signup', userController.signUp);
 router.post('/login', userController.login);
-router.post('/addToFavourites', userController.addEventToFavorites);
-router.post('/getUserHistory', userController.getUserHistory);
-router.post('/getEventsPosted', userController.getEventsPosted);
-router.post('/buyTickets', userController.buyTickets);
-router.post('/updateUserEventStatus', userController.updateUserEventStatus);
-router.get('/:id', userController.viewUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post('/addToFavourites', checkJwt, userController.addEventToFavorites);
+router.post('/getUserHistory', checkJwt, userController.getUserHistory);
+router.post('/getEventsPosted', checkJwt, userController.getEventsPosted);
+router.post('/buyTickets', checkJwt, userController.buyTickets);
+router.post('/updateUserEventStatus', checkJwt,userController.updateUserEventStatus);
+router.get('/:id' ,checkJwt, userController.viewUser);
+router.put('/:id', checkJwt, userController.updateUser);
+router.delete('/:id', checkJwt, userController.deleteUser);
 
 module.exports = router;
